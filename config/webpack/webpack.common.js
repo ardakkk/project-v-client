03dc151c9f609bc000965f21076ebb468e62a62c
ardakkk <a.tileu@infin-it.kz>
2019-02-18 21:36:22 +0600

    Configured file loader and webpack image loader

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -49,7 +49,14 @@ module.exports = {
                         loader: 'sass-loader'
                     }
                 ]
-            }
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]',
+                    'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
+                ],
+            },
         ]
     },
     plugins: [
--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import * as classes from  './Description.scss';
-
+const logo = require('../../assets/images/react.png');
 interface DescriptionProps {
 }
 
 export const Description: React.FunctionComponent<DescriptionProps> = (props) => {
+
     return (
         <div className={ classes.desc }>
+            <img src={ logo } alt='logo'/>
             <div className={ classes.item }>1</div>
             <div className={ classes.item }>2</div>
             <div className={ classes.item }>3</div>
