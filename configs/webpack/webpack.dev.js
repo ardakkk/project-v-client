5d6f5843405791e0572d277130c31bdc189b585b
Ardak Tileu <ardaktileu@MacBook-Pro-Ardak.local>
2019-03-24 01:46:35 +0600

    Adde tree shaking

--- configs/webpack/webpack.dev.js
+++ configs/webpack/webpack.dev.js
@@ -1,6 +1,7 @@
 const { resolve } = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const commonConfig = require('./webpack.common');
 
 module.exports = merge(
@@ -20,6 +21,18 @@ module.exports = merge(
         plugins: [
             new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
         ],
+        optimization: {
+            minimizer: [new UglifyJsPlugin({
+                chunkFilter: (chunk) => {
+                    // Exclude uglification for the `vendor` chunk
+                    if (chunk.name === 'vendor') {
+                        return false;
+                    }
+                    return true;
+                },
+                cache: true,
+            })],
+        },
         devServer: {
             port: 8080,
             hot: true,
--- configs/webpack/webpack.prod.js
+++ configs/webpack/webpack.prod.js
@@ -1,5 +1,6 @@
 const { resolve } = require('path');
 const merge = require('webpack-merge');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const commonConfig = require('./webpack.common');
 
 module.exports = merge(commonConfig, {
@@ -7,6 +8,18 @@ module.exports = merge(commonConfig, {
     entry: {
         vendor: ['react', 'react-dom'],
         bundle: './index.tsx'
+    },
+    optimization: {
+        minimizer: [new UglifyJsPlugin({
+            chunkFilter: (chunk) => {
+                // Exclude uglification for the `vendor` chunk
+                if (chunk.name === 'vendor') {
+                    return false;
+                }
+                return true;
+            },
+            cache: true,
+        })],
     },
     output: {
         filename: 'js/[name].[hash].min.js',
