a6eecdd0bb2424ba5de53afb43eadc985641d5ab
ardakkk <ardakfuse@gmail.com>
2019-12-01 22:05:32 +0600

    Fixed unit tests

--- src/components/molecules/MobileMenu/__tests__/MobileMenu.test.tsx
+++ src/components/molecules/MobileMenu/__tests__/MobileMenu.test.tsx
@@ -1,11 +1,17 @@
 import { shallow } from 'enzyme';
 import * as React from 'react';
+import { Provider } from 'react-redux';
 
-import { MobileMenu } from '../MobileMenu';
+import store from '../../../../store';
+import MobileMenu from '../MobileMenu';
 
 describe('MobileMenu component.', () => {
     it('Snapshot should match.', () => {
-        const component = shallow(<MobileMenu />);
+        const component = shallow(
+            <Provider store={store}>
+                <MobileMenu />
+            </Provider>
+        );
 
         expect(component).toMatchSnapshot();
     });
