fca386eb59ff317c00d3d48c9fb2dc5e1903485f
ardakkk <ardakfuse@gmail.com>
2020-02-09 21:20:18 +0600

    Client | Fixed type checking on store index file

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,7 +8,7 @@ export interface IApplicationState {
     darkMode: IDarkModeState;
 }
 
-const composeEnhancers =
+const composeEnhancers: any =
     process.env.NODE_ENV === 'development'
         ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
         : null || compose;
